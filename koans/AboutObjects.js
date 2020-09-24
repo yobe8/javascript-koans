bb2e86532dc549c9df7709676b3f32c903d03187
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-24 22:05:49 +0900

    updated AboutObjects

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -8,12 +8,12 @@ describe("About Objects", function () {
     });
 
     it("should confirm objects are collections of properties", function () {
-      expect(megalomaniac.mastermind).toBe(FILL_ME_IN);
+      expect(megalomaniac.mastermind).toBe('Joker');
     });
 
     it("should confirm that properties are case sensitive", function () {
-      expect(megalomaniac.henchwoman).toBe(FILL_ME_IN);
-      expect(megalomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(megalomaniac.henchwoman).toBe('Harley');
+      expect(megalomaniac.henchWoman).toBe(undefined);
     });
   });
 
@@ -24,12 +24,12 @@ describe("About Objects", function () {
       henchman: "Pinky",
       battleCry: function (noOfBrains) {
         return "They are " + this.henchman + " and the" +
-          Array(noOfBrains + 1).join(" " + this.mastermind);
+          Array(noOfBrains + 1).join(" " + this.mastermind); //5개 빈 배열을 ' brain'을 구분자로 해서 나누니 4개의 ' brain'이 문자열로 반환.
       }
     };
 
     var battleCry = megalomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
+    expect('They are Pinky and the Brain Brain Brain Brain').toMatch(battleCry);
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2020);
+    expect(megalomaniac.calculateAge()).toBe(50);
   });
 
   describe("'in' keyword", function () {
@@ -60,33 +60,33 @@ describe("About Objects", function () {
 
     it("should have the bomb", function () {
 
-      var hasBomb = "theBomb" in megalomaniac;
+      var hasBomb = "theBomb" in megalomaniac; // in 을 사용하면 객체 안의 프로퍼티 키의 존재 여부를 Boolean 값으로 반환.
 
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function () {
 
       var hasDetonator = "theDetonator" in megalomaniac;
 
-      expect(hasDetonator).toBe(FILL_ME_IN);
+      expect(hasDetonator).toBe(false);
     });
   });
 
   it("should know that properties can be added and deleted", function () {
     var megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
 
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(false);
 
     megalomaniac.secretary = "Agent Smith";
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(true);
 
     delete megalomaniac.henchman;
-    expect("henchman" in megalomaniac).toBe(FILL_ME_IN);
+    expect("henchman" in megalomaniac).toBe(false);
   });
 
 
-  it("should use prototype to add to all objects", function () {
+  it("should use prototype to add to all objects", function () { //prototype 짱 신기.
       function Circle(radius)
       {
         this.radius = radius;
@@ -96,14 +96,14 @@ describe("About Objects", function () {
       var colouredCircle = new Circle(5);
       colouredCircle.colour = "red";
 
-      expect(simpleCircle.colour).toBe(FILL_ME_IN);
-      expect(colouredCircle.colour).toBe(FILL_ME_IN);
+      expect(simpleCircle.colour).toBe(undefined);
+      expect(colouredCircle.colour).toBe('red');
 
       Circle.prototype.describe = function () {
         return "This circle has a radius of: " + this.radius;
       };
 
-      expect(simpleCircle.describe()).toBe(FILL_ME_IN);
-      expect(colouredCircle.describe()).toBe(FILL_ME_IN);
+      expect(simpleCircle.describe()).toBe('This circle has a radius of: 10');
+      expect(colouredCircle.describe()).toBe('This circle has a radius of: 5');
   });
 });
