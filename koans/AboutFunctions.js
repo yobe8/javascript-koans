77a8ed68b9a5162882c2b9bff67561a67150ce22
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-24 21:04:36 +0900

    Update AboutFunctions.js

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -6,7 +6,7 @@ describe("About Functions", function() {
       return a + b;
     }
 
-    expect(add(1, 2)).toBe(FILL_ME_IN);
+    expect(add(1, 2)).toBe(3);
   });
 
   it("should know internal variables override outer variables", function () {
@@ -21,9 +21,9 @@ describe("About Functions", function() {
       return message;
     }
 
-    expect(getMessage()).toBe(FILL_ME_IN);
-    expect(overrideMessage()).toBe(FILL_ME_IN);
-    expect(message).toBe(FILL_ME_IN);
+    expect(getMessage()).toBe('Outer');
+    expect(overrideMessage()).toBe('Inner');
+    expect(message).toBe('Outer');
   });
 
   it("should have lexical scoping", function () {
@@ -35,14 +35,14 @@ describe("About Functions", function() {
       }
       return childfunction();
     }
-    expect(parentfunction()).toBe(FILL_ME_IN);
+    expect(parentfunction()).toBe('local');
   });
 
-  it("should use lexical scoping to synthesise functions", function () {
+  it("should use lexical scoping to synthesise functions", function () { // https://poiemaweb.com/js-scope 포이에마웹 스코프 부분 참조.
 
     function makeMysteryFunction(makerValue)
     {
-      var newFunction = function doMysteriousThing(param)
+      var newFunction = function doMysteriousThing(param) //호이스팅과 관련된 거란다. 함수 선언식과 표현식의 차이로.
       {
         return makerValue + param;
       };
@@ -52,7 +52,7 @@ describe("About Functions", function() {
     var mysteryFunction3 = makeMysteryFunction(3);
     var mysteryFunction5 = makeMysteryFunction(5);
 
-    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(FILL_ME_IN);
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(23);
   });
 
   it("should allow extra function arguments", function () {
@@ -61,23 +61,23 @@ describe("About Functions", function() {
       return firstArg;
     }
 
-    expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnFirstArg("first", "second", "third")).toBe('first');
 
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
     }
 
-    expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+    expect(returnSecondArg("only give first arg")).toBe(undefined);
 
     function returnAllArgs() {
       var argsArray = [];
-      for (var i = 0; i < arguments.length; i += 1) {
+      for (var i = 0; i < arguments.length; i += 1) { //arguments.length는 함수 실행할 때 입력한 인수 길이를 반환.
         argsArray.push(arguments[i]);
       }
-      return argsArray.join(",");
+      return argsArray.join(","); // Array.join([separator]) 는 separator에 넣은 값을 배열 사이에 구분자로 넣어서 문자열로 반환.
     }
 
-    expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnAllArgs("first", "second", "third")).toBe('first,second,third');
   });
 
   it("should pass functions as values", function () {
@@ -90,11 +90,11 @@ describe("About Functions", function() {
       return name + " totally rules!";
     };
 
-    var praiseSinger = { givePraise: appendRules };
-    expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
+    var praiseSinger = { givePraise: appendRules }; //함수선언문을 값으로 사용.
+    expect(praiseSinger.givePraise("John")).toBe('John rules!');
 
-    praiseSinger.givePraise = appendDoubleRules;
-    expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
+    praiseSinger.givePraise = appendDoubleRules; //함수선언문 변경.
+    expect(praiseSinger.givePraise("Mary")).toBe('Mary totally rules!');
 
   });
 });
