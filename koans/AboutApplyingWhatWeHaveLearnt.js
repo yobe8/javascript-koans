198917879d16bbb8026ca8ebc95a53b2446eeba4
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-26 19:33:13 +0900

    Update AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -66,9 +66,10 @@ describe("About Applying What We Have Learnt", function() {
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = _.reduce(_.range(3,1000, 3), _.range(5,1000,5));    /* try chaining range() and reduce() */
+    var sum = FILL_ME_IN;
+    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(sum));
+    expect(233168).toBe(FILL_ME_IN);
   });
 
   /*********************************************************************************/
